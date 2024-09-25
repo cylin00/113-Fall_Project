cc0661d2dd72dece36e748b336f9a7a7e4d975bf
Chinying Lin <104905519+cylin00@users.noreply.github.com>
2024-09-25 19:40:14 +0800

    feat: finish step 1

--- QBH.py
+++ QBH.py
@@ -3,19 +3,21 @@ import numpy as np
 
 with wave.open('hummingdata/10/lugo_朋友.wav', 'rb') as audio:
     
+    # Step 0: Read the audio file and extract the amplitude data
+
     params = audio.getparams()
     num_channels, sample_width, frame_rate, num_frames = params[:4]
     
-    print(f"num_channels = {num_channels}")
-    print(f"sample_width = {sample_width}")
-    print(f"frame_rate = {frame_rate}")
-    print(f"num_frames = {num_frames}")
-
-
     frames = audio.readframes(num_frames)
-
-    audio_data = np.frombuffer(frames, dtype=np.int16)  # dtype depends on sample width
+    audio_data = np.frombuffer(frames, dtype=np.int16) 
+    amplitude_data = abs(audio_data)
     
-    print(f"Audio Data: {audio_data[:10]}")
-    print(f"Number of Channels: {num_channels}")
-    print(f"Frame Rate: {frame_rate}")
+    # Step 1: Select the max-amplitude data in groups of n0 = 80
+
+    n0 = 80
+
+    for i in range(0, len(amplitude_data), n0):
+        group = amplitude_data[i:i + n0]
+        A = max(group)
+    
+        print(f"Group {i // n0 }, Max Amplitude: {A}")
