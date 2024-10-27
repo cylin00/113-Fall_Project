3b44c257f75437b630c0b2bd1c02a9f9662aa76f
Chinying Lin <104905519+cylin00@users.noreply.github.com>
2024-10-27 15:28:47 +0800

    feat: draw the graph with DFT

--- QBH.py
+++ QBH.py
@@ -2,6 +2,10 @@ import wave
 import numpy as np
 import matplotlib.pyplot as plt
 
+def GetAmplitude(audio, n0, startTime, endTime):
+    segment = audio[int(startTime * frame_rate // n0):int(endTime * frame_rate // n0)]
+    return segment
+
 with wave.open('hummingdata/10/lugo_朋友.wav', 'rb') as audio:
     
     # Step 0: Read the audio file and extract the amplitude data
@@ -12,6 +16,9 @@ with wave.open('hummingdata/10/lugo_朋友.wav', 'rb') as audio:
     frames = audio.readframes(num_frames)
     audio_data = np.frombuffer(frames, dtype=np.int16) 
     amplitude_data = abs(audio_data)
+    time = np.linspace(0, len(audio_data) / frame_rate, num=len(audio_data))
+
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 6))
 
     # Step 1: Select the max-amplitude data in groups of n0 = 80
 
@@ -45,39 +52,132 @@ with wave.open('hummingdata/10/lugo_朋友.wav', 'rb') as audio:
     # Step 5: Convolution of B with an envelope match filter
         
     f = [3, 3, 4, 4, -1, -1, -2, -2, -2, -2, -2, -2]
-    C = np.zeros(len(B))
+    Enveloped_Data = np.zeros(len(B))
     for i in range(0, len(B)):
         for j in range(0, 11):
-            C[i] += B[i - j] * f[j]
+            Enveloped_Data[i] += B[i - j] * f[j]
         # print(f"C{i}: {C[i]}")
             
     # Step 6: Find the onsets of C and > threshold value
-    
-    threshold = 15
+    #print(f"data after chosen: {Enveloped_Data}")
+
+    threshold = 15.3
     t = 0
     Predicted_onsets = []
-    Predicted_onsets_values = []
-    for i in range(1, len(C) // 3 ):
-        if (C[3*i] > threshold) & (C[3*i] > C[3*i - 1]) & (C[3*i] > C[3*i - 2]):
+    for i in range(1, len(Enveloped_Data) // 3 ):
+        if (Enveloped_Data[3*i] > threshold) & (Enveloped_Data[3*i] > Enveloped_Data[3*i - 1]) & (Enveloped_Data[3*i] > Enveloped_Data[3*i - 2]):
             Predicted_onsets.append(3*i)
-            Predicted_onsets_values.append(C[3*i])
             t += 1
-    print(f"Number of onsets: {t}")
+            # print(f"{t}-th predicted onsets = {Enveloped_Data[3*i]}, index = {3*i}")
 
     # Draw the graph with onsets
     onset_times = np.array(Predicted_onsets) * n0 / frame_rate
-
-    plt.figure(figsize=(10, 4))
-    plt.plot(time, audio_data, label='Amplitude')
-
+    ax1.plot(time, audio_data)
     for onset_time in onset_times:
-        plt.axvline(x=onset_time, color='red', linestyle='-', label='Onset' if onset_time == onset_times[0] else "")
+         ax1.axvline(x=onset_time, color='red', linestyle='-', linewidth = 0.5, label='Onset' if onset_time == onset_times[0] else "")
+    ax1.set_title('Amplitude vs. Time with Onsets')
+    ax1.set_xlabel('time')
+    ax1.set_ylabel('Amplitude')
 
-    plt.title('Amplitude vs. Time with Onsets')
-    plt.ylabel('Amplitude')
-    plt.xlabel('Time [s]')
-    plt.grid()
-    plt.legend()
+
+    # Remove small intervals between onsets
+    for o in range(1, len(Predicted_onsets)):
+        previous_index = o - 1
+    
+        while previous_index >= 0 and Predicted_onsets[previous_index] == 0:
+            previous_index -= 1 
+
+        if previous_index >= 0:
+            if Predicted_onsets[o] - Predicted_onsets[previous_index] < 0.15 * frame_rate // n0:
+                Predicted_onsets[o] = 0
+
+
+    # Remove the last onset if interval < 0.15 sec
+    if Predicted_onsets[len(Predicted_onsets)-1] > (len(audio_data) - 0.15) * frame_rate // n0:
+        Predicted_onsets[len(Predicted_onsets)-1] = 0
+
+    # Draw the graph with new onsets
+    onset_times = np.array(Predicted_onsets) * n0 / frame_rate
+
+    # Store the onset times & onsets
+    OnsetTimes = []
+    for onset_time in onset_times:
+        if onset_time != 0:
+            OnsetTimes.append(onset_time)
+            print(f"onset_time: {onset_time} sec")
+    
+    OnsetIndex = []
+    for onsetIndex in Predicted_onsets:
+        if onsetIndex != 0:
+            OnsetIndex.append(onsetIndex)
+
+    # print(f"length of onset indexs = {len(OnsetIndex)}")
+
+    ax2.plot(time, audio_data)
+    for onset_time in OnsetTimes:
+        ax2.axvline(x=onset_time, color='red', linestyle='-', linewidth = 0.5, label='Onset' if onset_time == onset_times[0] else "")
+    ax2.set_title('Amplitude vs. Time with Onsets (After Filtered)')
+    ax2.set_xlabel('time')
+    ax2.set_ylabel('Amplitude')
+
+
+    ax3.plot(time, audio_data)
+    for onset_time in OnsetTimes:
+        ax3.axvline(x=onset_time, color='red', linestyle='-', linewidth = 0.5, label='Onset' if onset_time == onset_times[0] else "")
+    
+    EnvelopAmplitudeThreshold = 0.02
+    for o in range(0, len(OnsetTimes)-1):
+        SilenceDuration = OnsetTimes[o+1] - OnsetTimes[o]
+        if SilenceDuration >= 1.5:
+            StartTime = OnsetTimes[o]
+            EndTime = OnsetTimes[o+1]
+            print(f"Silence between {OnsetTimes[o-1]} and {OnsetTimes[o]}")
+            
+            amplitudes_in_silence = GetAmplitude(Enveloped_Data, n0, StartTime, EndTime)
+            #print(f"Amplitude in silence: {amplitudes_in_silence}")
+
+            # set adaptive threshold
+            newThreshold = threshold - 0.5
+            startIndex = 0
+            for s in range(0, len(amplitudes_in_silence)-1):
+                if(amplitudes_in_silence[s] >= newThreshold and (s - startIndex) >= (0.15 * frame_rate // n0)):
+                    startIndex = s
+                    ax3.axvline(x=StartTime + s * n0 / frame_rate, color='green', linestyle='-', linewidth = 0.5, label='Onset' if onset_time == onset_times[0] else "")
+                    print(f"new possible threshold = {amplitudes_in_silence[s]}, time = {StartTime + s * n0 / frame_rate}")
+    
+    print(f"predicted number of onsets = {len(OnsetTimes)}")
+
+    ax3.set_title('Amplitude vs. Time with Adaptive Threshold')
+    ax3.set_xlabel('time')
+    ax3.set_ylabel('Amplitude') 
+
+    plt.tight_layout()
     plt.show()
 
-    # Delete small intervals between onsets
+    # Discrete Fourier Transform 
+    
+    N = len(OnsetIndex)
+    fs = 8000
+
+    for o in range(0, N-1):
+        window_size = OnsetIndex[o+1] - OnsetIndex[o]
+        segment = Enveloped_Data[o:o+window_size]
+        X_m_segment = np.fft.fft(segment)
+        freqs_segment = np.fft.fftfreq(len(segment), 1/fs)
+        print(f"Window size = {window_size}, X_m_segment: {X_m_segment}")
+
+        magnitude = np.abs(X_m_segment)
+        indices = np.arange(OnsetIndex[o], OnsetIndex[o+1])
+        plt.figure(figsize=(10, 6))
+        plt.plot(indices, magnitude, label='DFT Magnitude', color='blue')
+        plt.plot(indices, segment, label='Original Data', color='orange', alpha=0.7)
+        plt.xlabel('Original Data Index')
+        plt.ylabel('DFT Magnitude')
+        plt.title('DFT Magnitude vs. Data Index')
+        plt.show()
+
+
+    # Add Smooth Filter to the DFT Result
+
+
+    # Denote the Fundamental Frequency
